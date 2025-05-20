0f9530244f49f04aa5030c9288fc205bfa26e9e9
BabickijDV <dimababickij50@gmail.com>
2025-05-20 12:41:16 +0300

    Delete I_LR3 directory

--- I_LR3/LR3/Task3/hexnumbercheck.py
+++
@@ -1,9 +0,0 @@
-def check(str):
-    """Check str on hex number
-
-    Return boolean
-    """
-    for c in str:
-        if not (c >= "0" and c <= "9" or c >= "A" and c <= "F"):
-            return False
-    return True
